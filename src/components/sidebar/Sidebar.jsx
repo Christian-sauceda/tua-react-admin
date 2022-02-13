d275fa360e12a511b083f0951684779f560aed2e
Christian-sauceda <81939528+Christian-sauceda@users.noreply.github.com>
2022-02-13 14:51:58 -0600

    dash

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,17 +1,11 @@
 import React from 'react'
-
 import { Link } from 'react-router-dom'
-
 import './sidebar.css'
-
 import logo from '../../assets/images/logo.png'
-
 import sidebar_items from '../../assets/JsonData/sidebar_routes.json'
 
 const SidebarItem = props => {
-
     const active = props.active ? 'active' : ''
-
     return (
         <div className="sidebar__item">
             <div className={`sidebar__item-inner ${active}`}>
@@ -25,9 +19,7 @@ const SidebarItem = props => {
 }
 
 const Sidebar = props => {
-
     const activeItem = sidebar_items.findIndex(item => item.route === props.location.pathname)
-
     return (
         <div className='sidebar'>
             <div className="sidebar__logo">
