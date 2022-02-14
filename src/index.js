0c15bc2365d5d465a931abece6fa6641ccc02450
Christian-sauceda <81939528+Christian-sauceda@users.noreply.github.com>
2022-02-14 14:04:24 -0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import reportWebVitals from './reportWebVitals'
-
 import { createStore } from 'redux'
-
 import { Provider } from 'react-redux'
-
 import rootReducer from './redux/reducers'
 
 import './assets/boxicons-2.0.7/css/boxicons.min.css'
